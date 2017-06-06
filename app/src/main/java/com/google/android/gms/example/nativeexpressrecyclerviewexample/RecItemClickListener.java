5d624e19d99910cd0e07451aabe20f3e041c607d
hjj <798085786@qq.com>
2017-06-06 10:26:39 +0800

    test

---
+++ app/src/main/java/com/google/android/gms/example/nativeexpressrecyclerviewexample/RecItemClickListener.java
@@ -0,0 +1,10 @@
+package com.google.android.gms.example.nativeexpressrecyclerviewexample;
+
+import android.view.View;
+
+/**
+ * Created by hjj on 2016/12/30.
+ */
+public interface RecItemClickListener {
+    void onclick(View v, int position);
+}
